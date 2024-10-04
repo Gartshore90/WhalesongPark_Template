b8d1880f4852ecadd5a8d01b88fd28ac9691666d
DEAN GARTSHORE <2200900@uad.ac.uk>
2024-10-04 13:50:49 +0100

    code added

---
+++ Assets/YOUR_STUFF_HERE/Assets/Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float forceStrength;
+    public Vector2 patrolPoint;
+    private Vector2 patrol;
+    private Rigidbody2D ourRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ourRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = patrolPoint - (Vector2)transform.position;
+        direction = direction.normalized;
+        ourRigidbody.AddForce(direction * forceStrength);
+    }
+}
